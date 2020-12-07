be3b79038d1d3763c51f6738440a21621a151721
moshe crespin <moshec315@gmail.com>
2020-12-07 14:55:16 +0200

    added load Json function

--- gameClient/myGame.java
+++ gameClient/myGame.java
@@ -15,12 +15,24 @@ import java.util.SplittableRandom;
 public class myGame {
 
     public static void main(String[] args) throws FileNotFoundException {
-        game_service game = Game_Server_Ex2.getServer(20);
-            System.out.println(loadForGames.loadJsonFromString(game.getGraph()));
-        System.out.println(game.getPokemons());
-        game.addAgent(0);
-        game.addAgent(2);
+        game_service game = Game_Server_Ex2.getServer(16);
+        game.startGame();
+
+//        System.out.println(game.timeToEnd());
+//        System.out.println(game.isRunning());
+        System.out.println(game.toString());
         System.out.println(game.getAgents());
+        System.out.println(game.getPokemons());
+
+//        System.out.println(game.getAgents());
+//        while (game.isRunning()) {
+//            System.out.println(loadForGames.LoadGraphFromJson(game.getGraph()));
+//            System.out.println(loadForGames.LoadPocimonsFromJson(game.getPokemons()));
+//            System.out.println(loadForGames.LoadAgentstsFromJson(game.getAgents()));
+//            game.addAgent(0);
+//            game.addAgent(2);
+//            System.out.println(game.getAgents());
+//        }
 
 
     }
